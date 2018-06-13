5d76643287cb90fe7a90c5933fe0cc9cdc51d00b
Sam Hamm <sam@codefellows.com>
2018-06-13 08:39:56 -0700

    Publishes lab

---
+++ starter-code/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//TODO: Finish out the server code according to the instructions in the lab README
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+// REVIEW: POST route needs to parse the body passed in with the request.
+// POST middleware
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/articles', (request, response) => {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(request.body);
+  response.status(201).json(request.body);
+})
