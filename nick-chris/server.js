a1b76fe17c7f7353e89d51a4d8e0861a1521ca04
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-06-13 16:31:04 -0700

    update articles works on server log

--- nick-chris/server.js
+++ nick-chris/server.js
@@ -19,27 +19,24 @@ app.use(express.static('./public'));
 // routes
 app.get('/kilovolt', (request, response) => {
   console.log('We got a kilovolt request!');
-  response.sendFile('public/', {root: '.'});
+  response.sendFile('./public/', {root: '.'});
 });
 
 app.get('/newblog', (request, response) => {
   console.log('You got something to say!'); 
-  response.sendFile('public/new.html', { root: '.'}); 
+  response.sendFile('./public/new.html', { root: '.'}); 
 }); 
+// Listener on a post
+app.post('/articles', (req, res) => {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(req.body);
+  res.status(201).json(req.body);
+});
+
 // 404 response !!!! 
 app.use((req, res, next) => {
   console.log(`Oh No, it's 404! Could not find file ${req.originalUrl}`);
   res.status(404).send(`<h1> Nick &amp; Chris are Cool!</h1><h2>You are a 404 fool! </h2><p>I pity the fool looking for </p> <code>  ${req.originalUrl} </code>`); 
 }); 
-
-// Listener on a post
-app.post('/articles', (request, response) => {
-  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
-  console.log(request.body);
-  response.status(201).json(request.body);
-});
-
-
-
 // Tell express to listen for the port activation
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
